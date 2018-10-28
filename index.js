d114c306d32a6f3ffed8b0d94fd65ff9536293d8
Jay Vazquez <jaybehckett@gmail.com>
2018-10-28 20:10:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ function whisper(string) { return string.toLowerCase() }
 function logShout (string) { console.log(string.toUpperCase()) }
 
 function logWhisper(string) { console.log(string.toLowerCase()) }
+
+function sayHiToGrandma(string) { 
