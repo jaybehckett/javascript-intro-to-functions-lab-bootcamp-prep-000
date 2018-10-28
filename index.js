4ee16e7dbc638d9131c803aa982d3218e5ba3850
Jay Vazquez <jaybehckett@gmail.com>
2018-10-28 20:19:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function logShout (string) { console.log(string.toUpperCase()) }
 
 function logWhisper(string) { console.log(string.toLowerCase()) }
 
-function sayHiToGrandma(string) { if ( string.toLowerCase() ) { return "I can't hear you!" } 
-else if ( string.toUpperCase() ) { return "YES INDEED!" } 
+function sayHiToGrandma(string) { if ( string.toLowerCase() === string.toLowerCase() ) 
+{ return "I can't hear you!" } 
+else if ( string.toUpperCase() === string.toUpperCase() ) 
+{ return "YES INDEED!" } 
   
 }
